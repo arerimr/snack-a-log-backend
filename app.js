87b04f70c54e9f0f4c9b452bb1f960e29f69c133
Diegocarrero <diegocar2413@gmail.com>
2023-05-08 18:53:03 -0400

    add app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+// const snackController = require("./controllers/snackController.js");
+// app.use("/snack", snackController);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our Snack-a-log App!");
+})
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+})
+
+module.exports = app;
